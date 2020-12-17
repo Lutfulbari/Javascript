6c25b6f4c0c047f316e06323eea7559e4414a77f
MOHAMMAD LUTFUL BARI <lutfulbarimusa@gmail.com>
2020-12-17 16:30:24 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+/*console.log("i am first line");
+console.log("i am second line");
+document.write("ja file is connected");*/
+/*document.write("ja file 'is' connected");
+document.write("ja file 'is' connected");
+document.write("ja file 'is' connected");
+document.write("ja file 'is' connected");
+document.write("ja file 'is' connected");
+document.write("ja file 'is' connected");
+document.write("ja file 'is' connected");
+*/
+
+/*var x;
+x = prompt("Enter your name: ");
+console.log(x);
+document.write(x);*/
+// document.write(67.14);
+// var a;
+// let b;
+// a = 45;
+// console.log(a);
+// a = 25.35;
+// console.log(a);
+// a = "lutful bari";
+
+// console.log(a);
+// // console.log(b);
+// var a = 20;
+// var b = a;
+// console.log(a);
+// console.log(b);
+// var a = 30;
+// console.log(a);
+// console.log(b);
+
+// var a = 20;
+// var b = 10;
+// var c = a+b;
+// console.log(c);
+
+// var $a;
+// $a = 50;
+// console.log($a);
+// var _abcd, avs3243;
+
+// var MYName, myName;
+// MyName = "abcd";
+// myName = "efgh";
+// console.log(MyName);
+// console.log(myName);
+// var a = 20;
+// console.log(a);
+// var a;
+// console.log(a);
+
+document.write("javascript reserved keywords list</br>")
+document.write("abstract,&nbsp;&nbsp;arguments,&nbsp;&nbsp;,boolean&nbsp;&nbsp;,barck </br>")
+document.write("byte,&nbsp;&nbsp;case&nbsp;&nbsp;,catch,char&nbsp;&nbsp;,")
+document.write("const,&nbsp;&nbsp;continue,&nbsp;&nbsp;debugger&nbsp;&nbsp;,default </br>")
+document.write("delete,do,doble,else,</br>")
+document.write("eval,false,final,finally,</br>")
+document.write("float,for,function,goto</br>")
+document.write("if,implements,in,instanceof,</br>")
+document.write("int,interface,let,long,</br>")
+document.write("native,new,null,package</br>")
+document.write("private,protected,public,return,</br>")
+document.write("short,static,switch,synchoronized,</br>")
+document.write("tnis,throw,throws,transient,</br>")
+document.write("true,try,typeof,var,</br>")
+document.write("void,volatile,while,with,</br>")
+document.write("yield,</br>")
+document.write("class,enum,export,extends,import,super</br>")
