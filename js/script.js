c0102bfabe6dc5d7ec99754d864817a9da39b550
MOHAMMAD LUTFUL BARI <lutfulbarimusa@gmail.com>
2020-12-17 16:41:07 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-/*console.log("i am first line");
+console.log("i am first line");
 console.log("i am second line");
-document.write("ja file is connected");*/
+document.write("ja file is connected");
 /*document.write("ja file 'is' connected");
 document.write("ja file 'is' connected");
 document.write("ja file 'is' connected");
@@ -10,48 +10,48 @@ document.write("ja file 'is' connected");
 document.write("ja file 'is' connected");
 */
 
-/*var x;
+var x;
 x = prompt("Enter your name: ");
 console.log(x);
-document.write(x);*/
-// document.write(67.14);
-// var a;
-// let b;
-// a = 45;
-// console.log(a);
-// a = 25.35;
-// console.log(a);
-// a = "lutful bari";
+document.write(x);
+document.write(67.14);
+var a;
+let b;
+a = 45;
+console.log(a);
+a = 25.35;
+console.log(a);
+a = "lutful bari";
 
-// console.log(a);
-// // console.log(b);
-// var a = 20;
-// var b = a;
-// console.log(a);
+console.log(a);
 // console.log(b);
-// var a = 30;
-// console.log(a);
-// console.log(b);
+var a = 20;
+var b = a;
+console.log(a);
+console.log(b);
+var a = 30;
+console.log(a);
+console.log(b);
 
-// var a = 20;
-// var b = 10;
-// var c = a+b;
-// console.log(c);
+var a = 20;
+var b = 10;
+var c = a+b;
+console.log(c);
 
-// var $a;
-// $a = 50;
-// console.log($a);
-// var _abcd, avs3243;
+var $a;
+$a = 50;
+console.log($a);
+var _abcd, avs3243;
 
-// var MYName, myName;
-// MyName = "abcd";
-// myName = "efgh";
-// console.log(MyName);
-// console.log(myName);
-// var a = 20;
-// console.log(a);
-// var a;
-// console.log(a);
+var MYName, myName;
+MyName = "abcd";
+myName = "efgh";
+console.log(MyName);
+console.log(myName);
+var a = 20;
+console.log(a);
+var a;
+console.log(a);
 
 document.write("javascript reserved keywords list</br>")
 document.write("abstract,&nbsp;&nbsp;arguments,&nbsp;&nbsp;,boolean&nbsp;&nbsp;,barck </br>")
